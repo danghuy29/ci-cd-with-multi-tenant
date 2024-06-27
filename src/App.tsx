e0a7824a660154b325d4229f37726aaa2db3560a
huydang-teq <huydang@teqnological.asia>
2024-06-27 09:46:37 +0700

    feat: add oidc

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>Deploy from github action</h2>
+      <h3>Authentication with OIDC</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
