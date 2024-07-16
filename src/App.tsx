c8906bb9e8cdb91bc7f1f7459813dfe2877f0105
huydang-teq <huydang@teqnological.asia>
2024-07-16 16:57:22 +0700

    deploy fina

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>Deploy from {import.meta.env.TENANT} LAPUTA</h2>
+      <h2>Deploy from {import.meta.env.TENANT} FINA</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
