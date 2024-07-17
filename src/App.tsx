65ad4f3bfbfc6e2b90669dfc223f31d4d03f8d18
huydang-teq <huydang@teqnological.asia>
2024-07-17 16:09:49 +0700

    deploy from secret

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>Deploy from {import.meta.env.TENANT}</h2>
+      <h2>Deploy from secret</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
